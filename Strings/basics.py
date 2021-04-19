480731397ef3beb912b7285c2c2dd62a160b0028
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-19 12:54:32 +0530

    strings basics

---
+++ Strings/basics.py
@@ -0,0 +1,32 @@
+# Creation of String  
+     
+# String with single quotes   
+print('Welcome to the Geeks World')
+     
+# String with double quotes  
+print("I'm a Geek")
+     
+# String with triple quotes
+print('''I'm a Geek and I live in a world of "Geeks"''')
+
+# Python Program to Access  
+# characters of String  
+     
+String1 = "GeeksForGeeks"
+     
+# Printing First character 
+print(String1[0])  
+     
+# Printing Last character 
+print(String1[-1])  
+
+# Update / delete in a String
+     
+String1 = "Hello, I'm a Geek"
+     
+# Updating a character
+String1[2] = 'p'
+   
+# Deleting a character   
+del String1[2]
+
