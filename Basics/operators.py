f4d6fa53353cbcacd0627e6d7c0a73648c7affc0
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-19 12:41:18 +0530

    first commit

---
+++ Basics/operators.py
@@ -0,0 +1,100 @@
+# Arithmetic Operator 
+a = 9
+b = 4
+   
+# Addition of numbers 
+add = a + b 
+# Subtraction of numbers  
+sub = a - b 
+# Multiplication of number  
+mul = a * b 
+# Division(float) of number  
+div1 = a / b 
+# Division(floor) of number  
+div2 = a // b 
+# Modulo of both number 
+mod = a % b 
+   
+# print results 
+print(add) 
+print(sub) 
+print(mul) 
+print(div1) 
+print(div2) 
+print(mod)
+
+
+#Relational Operators 
+a = 13
+b = 33
+   
+# a > b is False 
+print(a > b) 
+   
+# a < b is True 
+print(a < b) 
+   
+# a == b is False 
+print(a == b) 
+   
+# a != b is True 
+print(a != b) 
+   
+# a >= b is False 
+print(a >= b) 
+   
+# a <= b is True 
+print(a <= b)
+
+
+# Logical Operator 
+a = True
+b = False
+   
+# Print a and b is False 
+print(a and b) 
+   
+# Print a or b is True 
+print(a or b) 
+   
+# Print not a is False 
+print(not a)
+
+
+# Bitwise operators 
+a = 10
+b = 4
+   
+# Print bitwise AND operation   
+print(a & b) 
+   
+# Print bitwise OR operation 
+print(a | b) 
+   
+# Print bitwise NOT operation  
+print(~a) 
+   
+# print bitwise XOR operation  
+print(a ^ b) 
+   
+# print bitwise right shift operation  
+print(a >> 2) 
+   
+# print bitwise left shift operation  
+print(a << 2)
+
+
+# Identity and 
+# Membership operator
+ 
+ 
+a1 = 'GeeksforGeeks'
+b1 = 'GeeksforGeeks'
+ 
+# Identity operator
+print(a1 is not b1)
+print(a1 is b1)
+ 
+# Membership operator
+print("G" in a1)
+print("N" not in b1)
---
+++ Basics/variables.py
@@ -0,0 +1,12 @@
+# integer 
+age = 23                    
+   
+# A floating point 
+salary = 1456.8            
+   
+# A string   
+name = "Steve"             
+   
+print(age) 
+print(salary) 
+print(name) 
