21c02b6c7eae9ea0380add7bc087d96f55451b17
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-19 12:46:26 +0530

    second commit

---
+++ Basics/input/output.py
@@ -0,0 +1,26 @@
+# raw_input() 
+   
+g = raw_input("Enter your name : ") 
+print g
+
+# input() 
+   
+val = input("Enter your value: ") 
+print(val)
+
+
+# Printing Output to console
+   
+# One object is passed 
+print("GeeksForGeeks") 
+   
+x = 5
+# Two objects are passed 
+print("x =", x) 
+   
+# code for disabling the softspace feature  
+print('G', 'F', 'G', sep ='') 
+   
+# using end argument 
+print("Python", end = '@')   
+print("GeeksforGeeks") 
