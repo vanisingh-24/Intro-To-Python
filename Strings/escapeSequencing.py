f7bc7574cce11f76c76028ac8b9989b1a9fdd6d8
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-26 20:47:22 +0530

    escape sequencing

---
+++ Strings/escapeSequencing.py
@@ -0,0 +1,21 @@
+# Escaping Single Quote 
+String1 = 'I\'m a "Geek"'
+print(String1)
+  
+# Escaping Doule Quotes
+String1 = "I'm a \"Geek\""
+print(String1)
+  
+# Printing Paths with the use of Escape Sequences
+String1 = "C:\\Python\\Geeks\\"
+print(String1)
+
+
+# Printing Geeks in HEX
+String1 = "This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print(String1)
+
+# Using raw String to ignore Escape Sequences
+String1 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print(String1)
+
