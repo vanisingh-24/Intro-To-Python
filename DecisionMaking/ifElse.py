c72d373c8f92a18a32ed92ff958bb781d26fce76
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-27 21:28:50 +0530

    decision making

---
+++ DecisionMaking/ifElse.py
@@ -0,0 +1,8 @@
+# If-Else statement
+
+i = 20;
+if (i < 15):
+    print ("i is smaller than 15")
+else:
+    print ("i is greater than 15")
+print ("i'm not in if and not in else Block")
---
+++ DecisionMaking/nestedIf.py
@@ -0,0 +1,10 @@
+i = 10
+if (i == 10):
+    #  First if statement
+    if (i < 15):
+        print ("i is smaller than 15")
+    # Nested - if statement
+    if (i < 12):
+        print ("i is smaller than 12 too")
+    else:
+        print ("i is greater than 15")
