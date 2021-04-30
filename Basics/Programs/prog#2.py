100ee202179f47f55e605c84d4fc2f863f4a68d8
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-04-30 22:21:50 +0530

    operator programs

---
+++ Basics/Programs/prog#2.py
@@ -0,0 +1,14 @@
+# The provided code stub reads two integers, a and b, from STDIN.
+
+#Add logic to print two lines.
+#The first line should contain the result of integer division, a // b.
+#The second line should contain the result of float division, a / b.
+
+
+a = int(input())
+b = int(input())
+
+# Integer division
+print(a//b)
+# Float division
+print(a/b)
---
+++ Loops/Programs/prog#1.py
@@ -0,0 +1,6 @@
+# The provided code stub reads and integer, n , from STDIN. For all non-negative integers i<n, print i^2.
+
+n = int(input())
+
+for i in range(0,n):
+  print(i**2)
