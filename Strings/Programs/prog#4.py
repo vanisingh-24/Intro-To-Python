9cb736ead45056d698d3ff2d55798fc49d332128
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-04 22:21:22 +0530

    mutation

---
+++ Strings/Programs/prog#4.py
@@ -0,0 +1,13 @@
+# Read a given string, change the character at a given index and then print the modified string.
+
+def mutate_string(string, position, character):
+    a=list(string)
+    a[position]= character
+    string=''.join(a)
+
+    return string
+
+s = input()
+i, c = input().split()
+s_new = mutate_string(s, int(i), c)
+print(s_new)
