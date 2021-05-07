d662d30da255fbceb0c3eb0a67d509f096a6a573
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-07 22:09:39 +0530

    first class functions

---
+++ Functions/firstClassFunctions.py
@@ -0,0 +1,39 @@
+## First Class functions
+
+## 1. Functions are objects
+
+def shout(text):
+    return text.upper()
+
+print(shout('Hello'))
+
+yell = shout
+print(yell('Hello'))
+
+## 2. Functions can be passed as arguments to other functions
+
+def shout(text):
+    return text.upper()
+def whisper(text):
+    return text.lower()
+
+def greet(func):
+    greeting = func("""Hi, I am created by a function
+                    passed as an argument.""")
+    print(greeting)
+
+greet(shout)
+greet(whisper)
+
+## 3. Functions can return another function
+
+def create_adder(x):
+    def adder(y):
+        return x+y
+ 
+    return adder
+
+add_15 = create_adder(15)
+print(add_15(10))
+
+
