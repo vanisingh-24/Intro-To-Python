60ad765c3a7388b06d0c1a138064dd52f296af31
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-08 01:03:22 +0530

    default arguments

--- Functions/arguments.py
+++ Functions/arguments.py
@@ -9,6 +9,47 @@ def myFunc(x,y=50):
 # Driver Code
 myFunc(10)
 
+def student(firstname, lastname ='Mark', standard ='Fifth'):
+    print(firstname, lastname, 'studies in', standard, 'Standard')
+
+# Using mutable objects as default argument values in python
+
+def appendItem(itemName, itemList = []):
+    itemList.append(itemName)
+    return itemList
+
+print(appendItem('notebook'))
+print(appendItem('pencil'))
+print(appendItem('eraser'))
+
+def addItemToDict(itemName, quantity, itemList = {}):
+    itemList[itemName] = quantity
+    return itemList
+
+print(addItemToDictionary('notebook', 4))
+print(addItemToDictionary('pencil', 1))
+print(addItemToDictionary('eraser', 1))
+
+def appendItem(itemName, itemList=None):
+    if itemList == None:
+        itemList = []
+    itemList.append(itemName)
+    return itemList
+
+print(appendItem('notebook'))
+print(appendItem('pencil'))
+print(appendItem('eraser'))
+
+def addItemToDictionary(itemName, quantity, itemList = None):
+    if itemList == None:
+        itemList = {}
+    itemList[itemName] = quantity
+    return itemList
+
+print(addItemToDictionary('notebook', 4))
+print(addItemToDictionary('pencil', 1))
+print(addItemToDictionary('eraser', 1))    
+
 ## Keyword arguments
 
 def student(firstname, lastname):
@@ -16,6 +57,18 @@ def student(firstname, lastname):
 
 student(firstname="Steve", lastname="Rogers")
 student(lastname="Rogers", firstname="Steve")
+
+def student(firstname, lastname ='Mark', standard ='Fifth'):
+     print(firstname, lastname, 'studies in', standard, 'Standard')
+ 
+# 1 keyword argument
+student(firstname ='John')    
+ 
+# 2 keyword arguments                
+student(firstname ='John', standard ='Seventh') 
+ 
+# 2 keyword arguments
+student(lastname ='Gates', firstname ='John')    
 
 ## variable length arguments
 
