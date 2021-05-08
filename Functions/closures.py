fb0ee74712124e0fbe4fca3cee1246296b178154
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-09 00:31:49 +0530

    python closures

---
+++ Functions/closures.py
@@ -0,0 +1,15 @@
+## Python Closures
+
+def outerFunction(text):
+    text = text
+
+    def innerFunction():
+        print(text)
+
+    return innerFunction
+
+if __name__ == '__main__':
+    myFunction = outerFunction('Hey')
+    myFunction()
+
+    
