a042cf9135b48b9893b140abd6268d7fe05e4550
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-10 00:04:29 +0530

    write a function

---
+++ Functions/Programs/prog#1.py
@@ -0,0 +1,17 @@
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+def is_leap(year):
+  leap = False
+
+  if year%400 == 0:
+    return True
+  if year%100 == 0:
+    return False
+  if year%4 == 0:
+    return True
+  return False
+
+  return leap
+
+year = int(input())
+print(is_leap(year))
