48dfe5e207b7d0d233e9eb0c491682903b8d51d8
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-24 22:18:44 +0530

    difference of sets

---
+++ Set/Programs/prog#7.py
@@ -0,0 +1,13 @@
+# The students of District College have subscriptions to English and French newspapers.
+# Some students have subscribed only to English, some have subscribed only to French, and some have subscribed to both newspapers.
+# You are given two sets of student roll numbers.
+# One set has subscribed to the English newspaper, one set has subscribed to the French newspaper.
+# Your task is to find the total number of students who have subscribed to both newspapers.
+
+n = input()
+english = set(map(int, input().split()))
+
+b = input()
+french = set(map(int, input().split()))
+
+print (len(english.intersection(french)))
