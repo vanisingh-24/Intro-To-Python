f4c5f8ccc5fc0afca661204ee4b69faa6ea4e777
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-06-06 14:56:05 +0530

    class and instance variables

--- OOP/classesObjects.py
+++ OOP/classesObjects.py
@@ -38,3 +38,63 @@ class Person:
 
 p = Person('Vani')
 p.say_hi()
+
+## Class and Instance variables
+## variables with a value 
+## assigned in the class declaration, are class variables and
+## variables inside methods and constructors are instance
+## variables.
+
+# Defining instance variable using a constructor. 
+class Dog:
+
+  # class variable
+  animal = 'dog'
+
+  # init method or constructor
+  def __init__(self, breed, color):
+
+    # Instance variables
+    self.breed = breed
+    self.color = color
+
+# Instantiate Object
+Rodger = Dog("pug","brown")
+Buzo = Dog("Bulldog", "black")
+
+print("Rodger is a ", Rodger.animal)
+print("Breed:", Rodger.breed)
+print("Color:", Rodger.color)
+
+print('\nBuzo is a', Buzo.animal)
+print('Breed: ', Buzo.breed)
+print('Color: ', Buzo.color)
+
+# Accessing class variable using class name
+print(Dog.animal)
+
+# Defining instance variable using a normal method.
+
+class Dog:
+
+  # Class variable
+  animal = "dog"
+
+  # Init method or constructor
+  def __init__(self, breed):
+
+    # Instance variable
+    self.breed = breed
+
+  # Adds an instance variable
+  def setColor(self, color):
+    self.color = color
+
+  # Retrieves instance variable
+  def getColor(self):
+    return self.color
+
+# Driver Code
+Rodger = Dog("pug")
+Rodger.setColor("brown")
+print(Rodger.getColor())
