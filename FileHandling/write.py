18ec9d847cf433d5213c1dc4376abb4f27aeed40
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-06-12 18:32:41 +0530

    writing a file

---
+++ FileHandling/write.py
@@ -0,0 +1,60 @@
+## Writing to a file in Python
+
+file = open("data.txt", "w")
+L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+s = "Hello \n"
+
+# writing a string to file
+file.write(s)
+
+# Writing multiple strings to a file
+file.writelines(L)
+
+file.close()
+
+## Appending to a file
+
+# Append vs write mode
+file1 = open("myfile.txt", "w")
+L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+file1.writelines(L)
+file1.close()
+  
+# Append-adds at last
+file1 = open("myfile.txt", "a")  # append mode
+file1.write("Today \n")
+file1.close()
+  
+file1 = open("myfile.txt", "r")
+print("Output of Readlines after appending")
+print(file1.read())
+print()
+file1.close()
+  
+# Write-Overwrites
+file1 = open("myfile.txt", "w")  # write mode
+file1.write("Tomorrow \n")
+file1.close()
+  
+file1 = open("myfile.txt", "r")
+print("Output of Readlines after writing")
+print(file1.read())
+print()
+file1.close()
+
+## With statement
+# SYNTAX - with open filename as file
+
+# # write data to a file using with statement
+
+L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+
+with open("file.txt", "w") as file1:
+    file1.write("Hello \n")
+    file1.writelines(L)
+
+with open("file.txt", "r") as file1:
+    print(file1.read())
+
+
+
