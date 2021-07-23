fee8dfa01f68567da73635293c15d48870478d81
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-07-23 16:25:44 +0530

    merge k sorted lists

--- Leetcode/023_Merge_K_Sorted_Lists.py
+++ Leetcode/023_Merge_K_Sorted_Lists.py
@@ -32,4 +32,54 @@ class Solution:
         if l2:
             tail.next = l2
         return dummy.next
+
+# OR
+
+class Solution:
+    ## based on merge sort
+    # divide function
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
         
+        mid = len(lists)//2
+        
+        l = self.mergeKLists(lists[:mid])
+        r = self.mergeKLists(lists[mid:])
+        return self.merge(l, r)
+          
+    # merge function
+    def merge(self, l1, l2):
+        tail = dummy = ListNode()
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
+    
+# OR
+
+class Solution:
+    def mergeKLists(self, lists):
+        mergedList = []
+        dummy = head = ListNode(0)
+        
+        for l in lists:
+            while l:
+                mergedList.append(l.val)
+                l = l.next
+        for x in sorted(mergedList):
+            dummy.next = ListNode(x)
+            dummy = dummy.next
+        return head.next
