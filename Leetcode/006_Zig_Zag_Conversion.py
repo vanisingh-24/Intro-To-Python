d7cc6b64c85d1dcf601e540a57cd8ab3190e600e
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-07-30 23:05:13 +0530

    the next permutation

--- Leetcode/006_Zig_Zag_Conversion.py
+++ Leetcode/006_Zig_Zag_Conversion.py
@@ -9,6 +9,30 @@
 
 class Solution:
     def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        charArray = [[] for _ in range(numRows)]
+        row = 0
+        direction = -1
+        
+        for c in s:
+            charArray[row].append(c)
+            if row == 0 or row == numRows - 1:
+                direction *= -1
+                
+            row += direction
+            
+        result = ''
+        for arr in charArray:
+            result += ''.join(arr)
+                
+        return result
+
+# OR
+
+class Solution:
+    def convert(self, s, numRows):
         if numRows == 1:
             return s
         
