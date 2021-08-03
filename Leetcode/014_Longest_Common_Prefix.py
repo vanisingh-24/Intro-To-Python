32284072643371e2c1d856225a5bdbd0f637a352
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-08-03 22:13:01 +0530

    longest common prefix

--- Leetcode/014_Longest_Common_Prefix.py
+++ Leetcode/014_Longest_Common_Prefix.py
@@ -3,6 +3,41 @@
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        n = len(strs)
+        if n == 0:
+            return ""
+        
+        res = ""
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        limit = min(len(first), len(last))
+        
+        for i in range(limit):
+            if first[i] == last[i]:
+                res += first[i]
+            else:
+                break
+                
+        return res
+
+# OR
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+      
+        pre = strs[0]
+        
+        for i in strs:
+            while not i.startswith(pre):
+                pre = pre[:-1]
+        
+        return pre 
+
+# OR
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs) == 0:
             return("")
         if len(strs) == 1:
