0c3937056de4c7136102ea5d53c8aea913dbde57
Vani Singh <vanisingh6612@gmail.com>
2021-08-09 19:04:12 +0530

    climbing stairs

---
+++ Leetcode/070_Climbing_Stairs.py
@@ -0,0 +1,17 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        prev1 = 1
+        prev2 = 2
+        curr = 0
+        
+        for i in range(2,n):
+            curr = prev1 + prev2
+            prev1 = prev2
+            prev2 = curr
+        return curr
