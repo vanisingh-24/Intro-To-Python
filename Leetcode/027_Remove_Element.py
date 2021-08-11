889de9f2a7fa9ad480247cafdeae22794c8e0886
Vani Singh <vanisingh6612@gmail.com>
2021-08-11 16:10:56 +0530

    remove element

--- Leetcode/027_Remove_Element.py
+++ Leetcode/027_Remove_Element.py
@@ -5,4 +5,19 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         while nums.count(val):
             nums.remove(val)
+
+# OR
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        N = len(nums)
+        i, j = 0, 0
+        
+        while j < N:
+            if (nums[j] != val):
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+            
+        return i
         
