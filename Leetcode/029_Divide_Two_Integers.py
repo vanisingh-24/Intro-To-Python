9d2df8dde509df470a19d4d24eb6b21b04ee254b
Vani Singh <vanisingh6612@gmail.com>
2021-08-12 21:19:09 +0530

    divide two integers

--- Leetcode/029_Divide_Two_Integers.py
+++ Leetcode/029_Divide_Two_Integers.py
@@ -24,3 +24,30 @@ class Solution:
             output = -output
             
         return min(2147483647, max(-2147483648, output))
+
+# OR
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        # bit manipulation
+        # bitwise shift to the left is the equivalent of a multiplication by 2
+        if dividend == -2147483648 and divisor == -1: return 2147483647
+        # sign of result
+        sign = -1 if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0) else 1
+        
+        quotient = 0
+        # Take the absolute value
+        d = abs(dividend)
+        dv = abs(divisor)
+        # Loop until the  dividend is greater than divisor
+        while d >= dv:
+            # This represents the number of bits shifted or
+            # how many times we can double the number
+            shift = 0
+            while d >= (dv << shift):
+                shift += 1
+            # Add the number of times we shifted to the quotient
+            quotient += (1 << (shift - 1))
+            # Update the dividend for the next iteration
+            d -= dv << (shift - 1)
+        return -quotient if sign == -1 else quotient
