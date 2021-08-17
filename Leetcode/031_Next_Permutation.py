8034260bb1f316728a81db7043afae4ac48068fc
Vani Singh <vanisingh6612@gmail.com>
2021-08-17 20:34:17 +0530

    next premutation

--- Leetcode/031_Next_Permutation.py
+++ Leetcode/031_Next_Permutation.py
@@ -2,6 +2,38 @@
 # If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
 # The replacement must be in place and use only constant extra memory.
 
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        N = len(nums)
+        pivot = 0
+        
+        # Find pivot
+        for i in range(N-1, 0, -1):
+            if nums[i-1] < nums[i]:
+                pivot = i
+                break
+        if pivot == 0:
+            nums.sort()
+            return
+        
+        #Find swap which first no. > pivot
+        swap = N-1
+        while nums[pivot - 1] >= nums[swap]:
+            swap -= 1
+        
+        # swap
+        nums[swap], nums[pivot-1] = nums[pivot-1], nums[swap]
+        
+        # reverse from pivot
+        nums[pivot:] = sorted(nums[pivot:])
+        
+        
+
+# OR
+
 class Solution(object):
     def nextPermutation(self, nums):
         """
