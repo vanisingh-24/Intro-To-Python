77a51be22df1312a50dd643875a0f01111e8db26
Vani Singh <vanisingh6612@gmail.com>
2021-09-17 18:39:16 +0530

    balanced binary tree

---
+++ Leetcode/110_Balanced_Binary_Tree.py
@@ -0,0 +1,15 @@
+# Given a binary tree, determine if it is height-balanced.
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+            
+            left , right = dfs(root.left), dfs(root.right)
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+            
+            return [balanced, 1 + max(left[1], right[1])]
+        
+        return dfs(root)[0]
+        
