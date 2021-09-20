cf1ca1976d0ce9902106a6207c647bbe3746c46d
Vani Singh <vanisingh6612@gmail.com>
2021-09-21 00:36:30 +0530

    intersection of two arrays

---
+++ Leetcode/350_Intersection_Of_Two_Arrays2.py
@@ -0,0 +1,22 @@
+# Given two integer arrays nums1 and nums2, return an array of their intersection.
+# Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        i = 0
+        j = 0
+        result = []
+        nums1.sort()
+        nums2.sort()
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return result
+        
