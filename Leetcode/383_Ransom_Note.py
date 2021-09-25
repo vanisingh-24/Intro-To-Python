03a930df3192df72244e245fdeee1ea9cf219f0e
Vani Singh <vanisingh6612@gmail.com>
2021-09-25 18:32:38 +0530

    ransom note

---
+++ Leetcode/383_Ransom_Note.py
@@ -0,0 +1,17 @@
+# Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+# Each letter in magazine can only be used once in ransomNote.
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        r = sorted(list(ransomNote))
+        m = sorted(list(magazine))
+        
+        for char in m:
+            if r and char == r[0]:
+                r.pop(0)
+                
+        if r:
+            return False
+        else:
+            return True
+        
