1be6d5e3f876e4f50bfa1d0f4b75e7dc0cd37ce3
Vani Singh <vanisingh6612@gmail.com>
2021-10-03 21:56:21 +0530

    binary tree level order traversal

--- Leetcode/102_Binary_Tree_Level_Order_Traversal.py
+++ Leetcode/102_Binary_Tree_Level_Order_Traversal.py
@@ -2,6 +2,30 @@
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        queue = [root]
+        next_queue = []
+        level = []
+        result = []
+        while queue != []:
+            for root in queue:
+                level.append(root.val)
+                if root.left is not None:
+                    next_queue.append(root.left)
+                if root.right is not None:
+                    next_queue.append(root.right)
+            result.append(level)
+            level = []
+            queue = next_queue
+            next_queue = []
+        return result
+            
+            
+# OR
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         res = []
         
         q = collections.deque()
