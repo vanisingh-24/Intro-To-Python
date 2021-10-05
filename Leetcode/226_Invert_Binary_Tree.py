544834588486e087f13bdc63344d47dec3c357b0
Vani Singh <vanisingh6612@gmail.com>
2021-10-05 14:52:14 +0530

    invert binary tree

--- Leetcode/226_Invert_Binary_Tree.py
+++ Leetcode/226_Invert_Binary_Tree.py
@@ -2,6 +2,23 @@
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        # swap the children
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+        
+
+# OR
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root is None:
             return None
         
