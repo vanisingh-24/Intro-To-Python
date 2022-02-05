414e1ff5db9440ed99121c54fd4ab683d7e6a6a8
Vani Singh <vanisingh6612@gmail.com>
2022-02-06 00:38:01 +0530

    problem 4

--- DSA_450_Questions/Arrays/004_SortAnArrayOf0s1s2s.py
+++ DSA_450_Questions/Arrays/004_SortAnArrayOf0s1s2s.py
@@ -9,3 +9,28 @@ def sort012(arr,n):
 n = int(input())
 arr = list(map(int, input().split(' ')))
 sort012(arr,n)
+
+# By Brute force method using double traversal
+
+def sort012(arr, n):
+  zero = 0
+  one = 0
+  two = 0
+
+  for i in arr:
+    if i == 0:
+      zero += 1
+    if i == 1:
+      one += 1
+
+  for i in range(0, zero):
+    arr[i] = 0
+  for i in range(zero, zero+one):
+    arr[i] = 1
+  for i in range(zero+one, n):
+    arr[i] = 2
+  return arr
+
+n = int(input())
+arr = list(map(int, input().split(' ')))
+sort012(arr,n)
