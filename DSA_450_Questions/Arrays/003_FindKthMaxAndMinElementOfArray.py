7cf2d1028c8382b096b1bc64eb52d3c10bf321ab
Vani Singh <vanisingh6612@gmail.com>
2022-02-07 18:12:49 +0530

    quick select approach

--- DSA_450_Questions/Arrays/003_FindKthMaxAndMinElementOfArray.py
+++ DSA_450_Questions/Arrays/003_FindKthMaxAndMinElementOfArray.py
@@ -10,3 +10,28 @@ arr = list(map(int, input().split(' ')))
 k = int(input())
 kthSmallest(arr,k)
 
+# Quick Select Algorithm
+
+def kthSmallest(arr, k):
+        kth = len(arr) - k
+        
+        def quickSelect(l, r):
+            pivot, p = arr[r], l
+            for i in range(l, r):
+                if arr[i] <= pivot:
+                    arr[p], arr[i] = arr[i], arr[p]
+                    p += 1
+            arr[p], arr[r] = arr[r], arr[p]
+            
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return arr[p]
+        
+        return quickSelect(0, len(arr) - 1)
+
+arr = list(map(int, input().split(' ')))
+k = int(input())
+kthSmallest(arr,k)
