3e1ecc4e26bf931341df0a5a311922fcd6076ce9
Vani Singh <vanisingh6612@gmail.com>
2022-02-08 19:07:44 +0530

    problem 7

---
+++ DSA_450_Questions/Arrays/007_CyclicallyRotateAnArrayByOne.py
@@ -0,0 +1,15 @@
+## Write a program to cyclically rotate an array by one
+
+# Reversal algorithm
+
+def rotate(arr, n):
+  i = 0
+  j = n - 1
+  while i != j:
+    arr[i], arr[j] = arr[j] , arr[i]
+    i += 1
+  return arr
+
+arr = [1,2,3,4,5]
+n = len(arr)
+rotate(arr, n)
---
+++ DSA_450_Questions/Arrays/008_LargestSumContiguousSubarray.py
@@ -0,0 +1,16 @@
+## Largest Sum Contiguous Subarray
+
+# Kadane's Algorithm
+
+def maxSubArraySum(nums,N):
+  partial_sum = nums[0]
+  global_max = nums[0]
+        
+  for i in range(1, N):
+    partial_sum = max(partial_sum + nums[i], nums[i])
+    global_max = max(global_max, partial_sum)
+  return global_max
+
+nums = [-2, -3, 4, -1, -2, 1, 5, -3]
+N = len(nums)
+maxSubArraySum(nums, N)
