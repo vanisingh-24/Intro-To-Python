9b04855598095ad74ffe6f12efec9cc012326b35
Vani Singh <vanisingh6612@gmail.com>
2022-02-14 19:17:39 +0530

    problem fifteen

---
+++ DSA_450_Questions/Arrays/015_CountInversions.py
@@ -0,0 +1,69 @@
+## Given an array of integers. Find the Inversion Count in the array. 
+
+# Simple solution
+
+def inversionCount(self, arr, n):
+        count = 0
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                if arr[i] > arr[j]:
+                    count += 1
+        return count
+
+# merge sort
+
+def inversionCount(arr, n):
+  return mergeSort(arr, 0, n - 1)
+
+def merge(arr, left, mid, right):
+  i = left
+  j = mid
+  k = 0
+  invCount = 0
+  temp = [0 for x in range(right - left + 1)]
+ 
+  while (i < mid) and (j <= right):
+    if arr[i] <= arr[j]:
+      temp[k] = arr[i]
+      k += 1
+      i += 1
+    else:
+      temp[k] = arr[j]
+      invCount += mid - i
+      k += 1
+      j += 1
+ 
+  while i < mid:
+    temp[k] = arr[i]
+    k += 1
+    i += 1
+ 
+  while j <= right:
+    temp[k] = arr[j]
+    k += 1
+    j += 1
+ 
+  k = 0
+  for i in range(left, right + 1):
+    arr[i] = temp[k]
+    k += 1
+ 
+  return invCount
+ 
+ 
+def mergeSort(arr, left, right): 
+  invCount = 0
+ 
+  if right > left:
+    mid = (right + left) // 2
+ 
+    invCount = mergeSort(arr, left, mid)
+    invCount += mergeSort(arr, mid + 1, right)
+    invCount += merge(arr, left, mid + 1, right)
+ 
+  return invCount
+
+n = int(input())
+arr = list(map(int, input().split()))
+inversionCount(arr, n)
