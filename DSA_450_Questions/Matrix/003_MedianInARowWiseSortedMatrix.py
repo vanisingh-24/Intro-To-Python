eee9347f9819c1aff47abc31fa908bfb1a148091
Vani Singh <vanisingh6612@gmail.com>
2022-02-24 21:47:39 +0530

    problem 3 matrix

---
+++ DSA_450_Questions/Matrix/003_MedianInARowWiseSortedMatrix.py
@@ -0,0 +1,45 @@
+## Given a row wise sorted matrix of size RxC where R and C are always odd, find the median of the matrix.
+
+# Naive Solution
+
+def median(self, matrix, r, c):
+        b=[]
+        for i in matrix:
+            for j in i:
+                b.append(j)
+        b.sort()
+        c=len(b)
+        return b[c//2]
+
+# Binary Search
+
+class Solution:
+    def median(self, matrix, r, c):
+        left = 1
+        right = 2000
+        target = (r*c+1)//2
+        while(left<right):
+            mid = left + (right-left)//2
+            count = 0
+            for i in range(r):
+                count += self.count(matrix[i],mid+1)
+                
+            if count < target:
+                left = mid+1
+            else:
+                right = mid
+                
+        return left
+        
+    def count(self,arr,ele):
+        left = 0
+        right = len(arr)
+        while(left<right):
+            mid = left + (right-left)//2
+            if arr[mid] < ele:
+                left = mid+1
+            else:
+                right = mid
+                
+        return left
+
